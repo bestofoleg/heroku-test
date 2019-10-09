2eebcd42aa52d77b5b00196d7d14107c6238d2cc
bestofoleg <gothic777ggg>
2019-10-09 18:42:04 +0400

    view added

--- src/main/java/com/test/herokutest/controller/IndexController.java
+++ src/main/java/com/test/herokutest/controller/IndexController.java
@@ -1,12 +1,23 @@
 package com.test.herokutest.controller;
 
+import freemarker.template.utility.StringUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
+@RequestMapping
 public class IndexController {
-    @GetMapping(path = "/index")
-    public Test getIndexPage() {
-        return new Test("Hello!");
+    @GetMapping("index")
+    public String getIndex(@RequestParam(required = false) String name, Model model) {
+        if (StringUtils.isEmpty(name)) {
+            name = "world";
+        }
+        name += "!!!";
+        model.addAttribute("name", name);
+        return "index";
     }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,1 +1,10 @@
-<html> <body> <h1> Hello! </h1></body> </html>
+<html>
+<body>
+<form method="get" action="index" >
+    <input type="text" name="name" placeholder="name"/>
+    <input type="submit" value="send">
+</form>
+<br>
+<h1>Hello, ${name?if_exists}</h1>
+</body>
+</html>
